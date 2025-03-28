8ed5ec4e1829a70f31e516734c7aad86582845e2
MiliMarulich <mmarulich@mail.austral.edu.ar>
2025-03-28 12:54:46 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -3,4 +3,4 @@ def earth():
     y = "Barbados"
     print(f"The result of {X} comes first in the dictionary than {Y} is {x < y}")
     print(f"The result of {Y} comes first in the dictionary than {X} is {y < x}")
-earth()
+
